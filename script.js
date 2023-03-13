16cb39f31cacabd3401c79ec8f6fcd88d31747ce
KacSzu <szulckacper19@gmail.com>
2023-03-13 11:50:01 +0100

    Displaying a Map Marker

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
-
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
@@ -26,10 +25,23 @@ if (navigator.geolocation)
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-        .openPopup();
+      map.on(`click`, function (mapEvent) {
+        console.log(mapEvent);
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className:`running-popup`,
+            })
+          )
+          .setPopupContent(`workout`)
+          .openPopup();
+      });
     },
     function () {
       alert(`Could not get your location`);

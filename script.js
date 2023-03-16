3767fe89cfe92884067173e96ec080bf1865e8cf
KacSzu <szulckacper19@gmail.com>
2023-03-16 12:13:43 +0100

    Move to Marker On Click

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const inputElevation = document.querySelector('.form__input--elevation');
 class Workout {
   date = new Date();
   id = (Date.now() + ` `).slice(-10);
+  clicks = 0;
   constructor(coords, distance, duration) {
     this.coords = coords; // [lat,lng]
     this.distance = distance; // in km's
@@ -21,6 +22,9 @@ class Workout {
     this.description = `${this.type[0].toUpperCase()}${this.type.slice(1)} on ${
       months[this.date.getMonth()]
     } ${this.date.getDate()}`;
+  }
+  click() {
+    this.clicks++;
   }
 }
 class Running extends Workout {
@@ -62,6 +66,7 @@ class App {
 
     form.addEventListener(`submit`, this._newWorkout.bind(this));
     inputType.addEventListener(`change`, this._toggleElevationField);
+    containerWorkouts.addEventListener(`click`, this._moveToPopup.bind(this));
   }
   _getPosition() {
     if (navigator.geolocation)
@@ -94,10 +99,10 @@ class App {
   }
   _hideForm() {
     inputDistance.value =
-    inputCadence.value =
-    inputDuration.value =
-    inputElevation.value =
-      '';
+      inputCadence.value =
+      inputDuration.value =
+      inputElevation.value =
+        '';
     form.style.display = `none`;
     form.classList.add(`hidden`);
     setTimeout(() => (form.style.display = `grid`), 1000);
@@ -152,7 +157,6 @@ class App {
     this._renderWorkout(workout);
     // Hide form + clear input fields
     this._hideForm(workout);
-
   }
 
   _renderWorkoutMarker(workout) {
@@ -216,6 +220,21 @@ class App {
         <span class="workout__unit">m</span>
       </div>`;
     form.insertAdjacentHTML(`afterend`, html);
+  }
+  _moveToPopup(e) {
+    const workoutEl = e.target.closest(`.workout`);
+    console.log(workoutEl);
+    if (!workoutEl) return;
+    const workout = this.#workouts.find(
+      work => work.id === workoutEl.dataset.id
+    );
+    this.#map.setView(workout.coords, 13, {
+      animate: true,
+      pan: {
+        duration: 1,
+      },
+    });
+    workout.click()
   }
 }
 
